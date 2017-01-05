450de4c340db2dc5ba2aca44d529d99f414c9ffa
Gojs <djalekzse@gmail.com>
2017-01-05 15:15:33 +0100

    Improved help text

--- csv2latex/help.py
+++ csv2latex/help.py
@@ -18,16 +18,18 @@ def help(level):
               "  --transpose-after \t Transpose after splitting \n"
               "                    \t (equivalent to -t if no splits)\n"
               "  --transpose-individual Step by step determine if the table should be\n"
-              "                         transposed or not\n"
+              "                         transposed or not (Not available)\n"
               "  -r, -rows <rows> \t Indicate at which rows to split and start a new table\n"
               "  -c, -columns <colums> \t Indicate at which columns to split and start\n"
               "              \t\t a new table\n"
-              ""
-              "  -l, --literal \t Do not escape LaTeX syntax")
+              "\n"
+              "  -l, --literal \t Do not escape LaTeX syntax (Not available)\n"
+              "  -p, --print \t\t Print resulting LaTeX table(s) to terminal instead\n"
+              "              \t\t of to file. Ignores -o option")
     if level >= 2:
         print("\nInstructions:\n"
-              "  -r 3,5,6 \t\t Splits into three row partitions\n"
-              "           \t\t 0 to 2, 3 to 5, and 6 to the final row")
+              "  -r 3,5,6 \t\t Splits into four row partitions\n"
+              "           \t\t 0 to 2, 3 to 4, only row 5, and 6 to the final row")
         print("\nFor further help, visit https://github.com/aekh/csv2latex/wiki")
     if level > 0:
         print("\nReport bugs at https://github.com/aekh/csv2latex/issues.")
